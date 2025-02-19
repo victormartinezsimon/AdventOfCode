4d4ce189048b85b8a6f6bc88410da41a50eb9298
PC-VICTOR\Victor <victormartinezsimon@gmail.com>
2025-02-19 23:52:33 +0100

    add day 17

--- 2019/2019.cpp
+++ 2019/2019.cpp
@@ -3446,6 +3446,7 @@ void day16_B(const std::string& number_str)
     std::cout << "\n";
 }
 
+
 void day16()
 {
     auto number_str = ReadFile("./input/day16.txt")[0];
@@ -3454,6 +3455,587 @@ void day16()
 
     day16_B(number_str);
 
+}
+
+class day17_intcode
+{
+public:
+    using memoryType = long long;
+    using inputFunction = function<memoryType()>;
+    using outputFunction = function<void(memoryType)>;
+
+private:
+    int instructionIndex = 0;
+    memoryType relativeBase = 0;
+    inputFunction inputFun;
+    outputFunction outputFun;
+    std::map<memoryType, memoryType> memory;
+    bool forceStop = false;
+
+    memoryType calculateInstruction(int operation, std::vector<memoryType> params)
+    {
+        switch (operation)
+        {
+        case 1: return params[0] + params[1]; break;
+        case 2: return params[0] * params[1]; break;
+        case 3: return -1; break;
+        case 4: return -1; break;
+        case 5: return params[0] != 0; break;
+        case 6: return params[0] == 0; break;
+        case 7: return params[0] < params[1]; break;
+        case 8: return params[0] == params[1]; break;
+        case 9: return -1; break;
+        }
+
+        std::cout << "error\n";
+        return 0;
+    }
+
+    memoryType getValue(memoryType value, int type)
+    {
+        switch (type)
+        {
+        case 0: return memory[value];
+        case 1: return value;
+        case 2:
+            if (memory.find(value + relativeBase) == memory.end()) {
+                memory[value + relativeBase] = 0;
+            }
+            return memory[value + relativeBase];
+        }
+        std::cout << "error \n";
+        return 0;
+    }
+
+    std::vector<int> parseParamsInfo(int value, int totalElements)
+    {
+        std::vector<int> result;
+
+        int instruction = value % 100;
+        result.push_back(instruction);
+        totalElements -= 2;
+
+        value = value / 100;
+        while (totalElements >= 0)
+        {
+            int val = value % 10;
+            result.push_back(val);
+            value = value / 10;
+            --totalElements;
+        }
+
+        return result;
+    }
+
+    int getParametersForOperation(int operation)
+    {
+        switch (operation)
+        {
+        case 1:
+        case 2: return 3;
+        case 3:
+        case 4: return 1;
+        case 5:
+        case 6: return 2;
+        case 7:
+        case 8: return 3;
+        case 9: return 1;
+        }
+        return 0;
+    }
+
+    std::vector<memoryType> readParametersFromInstruction(int operation)
+    {
+        int totalParams = getParametersForOperation(operation);
+        std::vector<memoryType> result;
+        for (int i = 0; i < totalParams; ++i)
+        {
+            auto value = memory[instructionIndex + i + 1];
+            result.push_back(value);
+        }
+        return result;
+    }
+    std::vector<memoryType> transformParameters(int operation, const std::vector<memoryType>& params, const std::vector<int>& parameterInfo)
+    {
+        std::vector<memoryType> result;
+        for (int i = 0; i < params.size(); ++i)
+        {
+            auto value = getValue(params[i], parameterInfo[i]);
+            result.push_back(value);
+        }
+
+        //direct result
+        std::set<int> directResult = { 1, 2, 3, 7, 8 };
+        if (directResult.find(operation) != directResult.end())
+        {
+            if (parameterInfo[result.size() - 1] == 2)
+            {
+                result[result.size() - 1] = params.back() + relativeBase;
+            }
+            else
+            {
+                result[result.size() - 1] = params.back();
+            }
+        }
+
+        return result;
+    }
+
+    void writeResult(memoryType result, int operation, std::vector<memoryType>& paramsTransformed)
+    {
+        if (operation == 5 || operation == 6)
+        {
+            return;
+        }
+
+        if (operation == 4)
+        {
+            outputFun(paramsTransformed[0]);
+            return;
+        }
+        if (operation == 9)
+        {
+            relativeBase += paramsTransformed[0];
+            return;
+        }
+
+        if (operation == 3)
+        {
+            result = inputFun();
+        }
+
+        int indexSave = paramsTransformed.back();
+        memory[indexSave] = result;
+    }
+
+    void updateIndex(std::vector<memoryType>& paramsTransformed, int operation, int result)
+    {
+        if (operation == 5 || operation == 6)
+        {
+            if (result)
+            {
+                instructionIndex = paramsTransformed[1];
+                return;
+            }
+        }
+
+        int totalParams = getParametersForOperation(operation);
+        instructionIndex += totalParams + 1;
+    }
+    //
+    //1:+
+    //2:*
+    //3:Read
+    //4:Write
+    //5:Jmp !=
+    //6:Jmp ==
+    //7:<
+    //8: ==
+    //9: +RelativeBase
+    void runProgram()
+    {
+        while (memory[instructionIndex] != 99)
+        {
+            if (forceStop)
+            {
+                return;
+            }
+
+            runOneInstruction();
+        }
+    }
+
+    void runOneInstruction()
+    {
+        auto instruction = memory[instructionIndex];
+        auto paramsInfo = parseParamsInfo(memory[instructionIndex], 5);
+        int operation = paramsInfo[0];
+        paramsInfo.erase(paramsInfo.begin());
+        auto paramsForInstruction = readParametersFromInstruction(operation);
+        auto paramsTransformed = transformParameters(operation, paramsForInstruction, paramsInfo);
+
+        auto result = calculateInstruction(operation, paramsTransformed);
+
+        writeResult(result, operation, paramsTransformed);
+        updateIndex(paramsTransformed, operation, result);
+    }
+
+    std::map<memoryType, memoryType> buildMemory(const std::string& txt)
+    {
+        auto split_value = split(txt, ",");
+        std::map<memoryType, memoryType> memory;
+
+        int index = 0;
+        for (auto v : split_value)
+        {
+            memory[index] = stoll(v.c_str());
+            ++index;
+        }
+
+        return memory;
+    }
+
+public:
+
+    void Init(const std::string& txt, inputFunction input, outputFunction output)
+    {
+        inputFun = input;
+        outputFun = output;
+        memory = buildMemory(txt);
+        instructionIndex = 0;
+        relativeBase = 0;
+    }
+
+    void Run()
+    {
+        runProgram();
+    }
+
+    void RunDebug()
+    {
+        runOneInstruction();
+    }
+
+    std::vector<int> GetMemoryIndexes()
+    {
+        std::vector<int> toReturn;
+        for (auto kvp : memory)
+        {
+            toReturn.push_back(kvp.first);
+        }
+        return toReturn;
+    }
+
+    memoryType GetValueInMemoryPosition(int index)
+    {
+        return memory[index];
+    }
+
+    void SetMemoryValue(memoryType value, memoryType index)
+    {
+        memory[index] = value;
+    }
+
+    void ForceStop()
+    {
+        forceStop = true;
+    }
+};
+
+std::vector<std::string> day17_getBoard(std::string& code)
+{
+    day17_intcode program;
+
+    std::vector<std::string> toReturn;
+    std::string currentString;
+
+    day17_intcode::inputFunction input = [] {
+
+        return 0;
+    };
+
+    day17_intcode::outputFunction output = [&toReturn, &currentString](day17_intcode::memoryType value)
+    {
+        if (value == 10)
+        {
+            if (!currentString.empty())
+            {
+                toReturn.push_back(currentString);
+            }
+            currentString.clear();
+        }
+        else
+        {
+            currentString += static_cast<char>(value);
+        }
+    };
+
+    program.Init(code, input, output);
+
+    program.Run();
+
+    return toReturn;
+}
+
+void day17_a(const std::vector<std::string>& board)
+{
+    int height = board.size();
+    int width = board[0].size();
+
+    int count = 0;
+
+    for (int row = 0; row < height; ++row)
+    {
+        for (int col = 0; col < width; ++col)
+        {
+            if (board[row][col] == '.') { continue; }
+
+            int tmp_count = 0;
+
+            std::pair<int, int> currentPos = { row, col };
+
+            for (Directions dir : {Directions::NORTH, Directions::SOUTH, Directions::EAST, Directions::WEST})
+            {
+                auto next = getNextPosition(currentPos, dir);
+                if (insideField(next, width, height) )
+                {
+                    auto value = board[next.first][next.second];
+                    if (value != '.')
+                    {
+                        ++tmp_count;
+                    }
+                }
+            }
+
+            if (tmp_count == 4)
+            {
+                count += (row * col);
+            }
+        }
+    }
+    
+    std::cout << "day 17 => " << count << "\n";
+}
+
+struct day17_node
+{
+    std::pair<int, int> position;
+    std::string result;
+    int countConsecutive = 0;
+    Directions currentDir;
+};
+
+std::string day17_getPath(const std::vector<std::string>& board)
+{
+    std::string result;
+    int width = board[0].size();
+    int height = board.size();
+
+    std::pair<int, int> start;
+    Directions dirStart = Directions::NONE;
+
+    for (int row = 0; row < height; ++row)
+    {
+        for (int col = 0; col < width; ++col)
+        {
+            if (board[row][col] == '.') { continue; }
+            if (board[row][col] == '#') { continue; }
+
+            start = { row, col };
+            switch (board[row][col])
+            {
+            case  '^': dirStart = Directions::NORTH; break;
+            case  'v': dirStart = Directions::SOUTH; break;
+            case '<': dirStart = Directions::WEST; break;
+            case '>': dirStart = Directions::EAST; break;
+            }
+
+            col = width;
+            row = height;
+        }
+    }
+
+    std::vector<std::vector<bool>> visited(std::vector<std::vector<bool>>(height, std::vector<bool>(width, false)));
+
+    std::vector<day17_node> toVisit;
+    day17_node startNode;
+    startNode.position = start;
+    startNode.countConsecutive = 0;
+    startNode.result = "";
+    startNode.currentDir = dirStart;
+    toVisit.push_back(startNode);
+
+    while (!toVisit.empty())
+    {
+        auto node = toVisit.front();
+        toVisit.erase(toVisit.begin());
+
+        //visited[node.position.first][node.position.second] = true;
+
+        int directionsTaken = 0;
+
+        //options => continue in same direction
+        //turn right + continue in this new direction
+        //turn left + continue in this new direction
+
+        {
+            auto positionSameDir = getNextPosition(node.position, node.currentDir);
+            if (insideField(positionSameDir, width, height) && board[positionSameDir.first][positionSameDir.second] != '.')
+            {
+                day17_node nextNode;
+                nextNode.countConsecutive = node.countConsecutive + 1;
+                nextNode.currentDir = node.currentDir;
+                nextNode.position = positionSameDir;
+                nextNode.result = node.result;
+
+                toVisit.push_back(nextNode);
+                continue;
+            }
+        }
+
+        //here we can turn right(90) or left(-90)
+        {
+            auto directionRight = turn90Degress(node.currentDir);
+            auto positionRight = getNextPosition(node.position, directionRight);
+            if (insideField(positionRight, width, height) && board[positionRight.first][positionRight.second] != '.')
+            {
+                std::string resTmp = node.result;
+                if (node.countConsecutive > 0)
+                {
+                    resTmp += std::to_string(node.countConsecutive) + ",";
+                }
+                resTmp += "R,";
+                day17_node nextNode;
+                nextNode.countConsecutive = 0;
+                nextNode.currentDir = directionRight;
+                nextNode.position = node.position;
+                nextNode.result = resTmp;
+                toVisit.push_back(nextNode);
+                continue;
+            }
+        }
+        {
+            auto directionLeft = turnMinus90Degress(node.currentDir);
+            auto positionLeft = getNextPosition(node.position, directionLeft);
+            if (insideField(positionLeft, width, height) && board[positionLeft.first][positionLeft.second] != '.')
+            {
+                std::string resTmp = node.result;
+                if (node.countConsecutive > 0)
+                {
+                    resTmp += std::to_string(node.countConsecutive) + ",";
+                }
+                resTmp += "L,";
+                day17_node nextNode;
+                nextNode.countConsecutive = 0;
+                nextNode.currentDir = directionLeft;
+                nextNode.position = node.position;
+                nextNode.result = resTmp;
+                toVisit.push_back(nextNode);
+                continue;
+            }
+        }
+
+        //no more decisions
+        result = node.result + std::to_string(node.countConsecutive);
+    }
+
+    return result;
+}
+
+
+void day17_b(const std::vector<std::string>& board, const std::string& code)
+{
+    auto paths = day17_getPath(board);
+    //L,6,L,4,R,12,L,6,R,12,R,12,L,8,L,6,L,4,R,12,L,6,L,10,L,10,L,6,L,6,R,12,R,12,L,8,L,6, L,4,R,12,L,6,L,10,L,10,L,6,L,6,R,12,R,12,L,8,L,6,L,4,R,12,L,6,L,10,L,10,L,6
+    //after doing manually, I get this programs and sequence
+    //#A#, #B#, #A#, #C#, #B#, #A#, #C#, #B#, #A#, #C#,
+    //where
+    //A: L, 6, L, 4, R, 12,
+    //B : L, 6, R, 12, R, 12, L, 8,
+    //C : L, 6, L, 10, L, 10, L, 6
+
+    /*
+    
+
+    std::vector<std::string> mainRutine =
+    { "A", ",", "B", ",", "A", ",", "C", ",", "B", ",", "A", ",", "C", ",", "B", ",", "A", ",", "C" };
+
+    std::vector<std::string> routineA =
+    { "L", ",", "6", ",", "L", ",", "4", ",", "R", ",", "12" };
+
+    std::vector<std::string> routineB = { "L", ",", "6", ",", "R", ",", "12", ",", "R", ",", "12", ",", "L", ",", "8" };
+    std::vector<std::string> routineC = { "L", ",", "6", ",", "L", ",", "10", ",", "L", ",", "10", ",", "L", ",", "6" };
+    std::vector<std::string> routineVideo = { "n" };
+
+    std::vector<std::vector<std::string>> inputs = { mainRutine, routineA, routineB, routineC, routineVideo };
+
+
+    int currentIndex = 0;
+    int currentInputIndex = 0;
+    day17_intcode program;
+
+    std::vector<std::string> board_str;
+    std::string currentString;
+
+    long long result;
+
+    day17_intcode::inputFunction input = [&currentIndex, &inputs, &currentInputIndex]
+    {
+        if (currentInputIndex >= inputs.size()) { return 10; }
+
+        if (currentIndex >= inputs[currentInputIndex].size())
+        {
+            currentIndex = 0;
+            ++currentInputIndex;
+            return 10;
+        }
+    };
+    
+    */
+
+
+    std::string mainRutine = "A,B,A,C,B,A,C,B,A,C";
+    std::string routineA = "L,6,L,4,R,12";
+    std::string routineB = "L,6,R,12,R,12,L,8";
+    std::string routineC = "L,6,L,10,L,10,L,6";
+
+    std::string input_txt = mainRutine + static_cast<char>(10) + routineA + static_cast<char>(10) + routineB + static_cast<char>(10) + routineC + static_cast<char>(10) + "n" + static_cast<char>(10);
+
+    int currentIndex = 0;
+    day17_intcode program;
+
+    std::vector<std::string> board_str;
+    std::string currentString;
+
+    long long result;
+
+    day17_intcode::inputFunction input = [&currentIndex, &input_txt]
+    {
+        char c = input_txt[currentIndex];
+        ++currentIndex;
+        return c;
+    };
+
+    day17_intcode::outputFunction output = [&result, &currentString, &board_str](day17_intcode::memoryType value)
+    {
+        if (value == 10)
+        {
+            if (!currentString.empty())
+            {
+                board_str.push_back(currentString);
+            }
+            currentString.clear();
+        }
+        else
+        {
+            currentString += static_cast<char>(value);
+        }
+
+        result = value;
+    };
+
+    program.Init(code, input, output);
+    program.SetMemoryValue(2, 0);
+
+    program.Run();
+
+    std::cout << "day17_B => " << result << "\n";
+
+}
+void day17()
+{
+    auto code = ReadFile("./input/day17.txt")[0];
+
+    auto board = day17_getBoard(code);
+
+    
+    for (auto l : board)
+    {
+        std::cout << l << "\n";
+    }
+    
+    day17_a(board);
+    day17_b(board, code);
 }
 
 int main()
@@ -3473,7 +4055,8 @@ int main()
     //day13();
     //day14();
     //day15();
-    day16();
+    //day16();
+    day17();
 }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
